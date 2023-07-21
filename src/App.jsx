3846217ce75508f18a24dfff3fc1c0b8cfd7b3ea
Jakub Buczyński <72788108+BuczekB@users.noreply.github.com>
2023-07-21 18:43:46 +0200

    init project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './style/App.scss'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='mainContainer'>
+          <h1>Netflix Clone</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
